09ea55121066fe2ff562c7bbee3320a84aa0bb76
Prateek sharma <pratiktech28@gmail.com>
2025-11-16 17:31:08 +0530

    Create password.py

---
+++ password.py
@@ -0,0 +1,10 @@
+# password-generator.py
+this is project in python password generator
+import random
+chars = "jdkdisosxnsxmxnxbbhweuhjdnmckdoeiwun"
+length = int(input("Entered length:"))
+password = ""
+
+for i in range(length):
+    password += random.choice(chars)
+    print("Generated password" , password)
